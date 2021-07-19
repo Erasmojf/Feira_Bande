9c965975d51e05a0f5102a148ff1ee74df90ab35
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-07-19 15:58:20 -0300

    CM -  Let’s create a custom navigation bar with a logo
     Let’s create a custom navigation bar with a logo

--- FeiraBande/FeiraBande/View/Home/FooterView.swift
+++ FeiraBande/FeiraBande/View/Home/FooterView.swift
@@ -3,7 +3,9 @@
 //  FeiraBande
 //
 //  Created by Erasmo J.F Da Silva on 19/07/21.
-//
+//  SwiftUI ♡ Better Apps. Less Code
+//  https://erasmojf.github.io/
+//  Fidju de Bideira de Feira de Caracol
 
 import SwiftUI
 
---
+++ FeiraBande/FeiraBande/View/Home/LogoView.swift
@@ -0,0 +1,45 @@
+//
+//  LogoView.swift
+//  FeiraBande
+//
+//  Created by Erasmo J.F Da Silva on 19/07/21.
+//  SwiftUI ♡ Better Apps. Less Code
+//  https://erasmojf.github.io/
+//  Fidju de Bideira de Feira de Caracol
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BBODY
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Feira".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Bandé".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }// HStack
+    }
+}
+
+// MARK: - PREVIEW
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
